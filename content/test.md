6e16b0745901151f2eff6ab8597df038595bb0ff
shihono <honobono.pika@gmail.com>
2017-11-16 13:59:14 +0900

    change theme

---
+++ content/test.md
@@ -0,0 +1,9 @@
+Title: Test Page
+Date: 2017-9-25
+Slug: test_page
+Authors: Hono Shirai
+Summary: Test Page
+
+# About
+
+* this is test page
---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'shihono'
+SITENAME = 'My page'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Japan'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Github link', 'http://github.com/shihono'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+THEME = "pelican-themes/apricot"
+GITHUB_URL = 'http://github.com/shihono'
+FAVICON = 'favicon.ico'
+FAVICON_TYPE = 'image/vnd.microsoft.icon'
+
+STATIC_PATHS = ['images', 'extra']
+EXTRA_PATH_METADATA = {
+    'extra/' + FAVICON: {'path': FAVICON},
+}
